61d7d4f2948d2f4ca9aa3f2f264ea5e4507e7db9
Usuario Autenticos <contacto@autenticos.co>
2026-05-05 22:15:36 -0500

    UI: Insert YouTube video vDMdczIxJ9s in the benefits section

--- src/components/RemainingComponents.jsx
+++ src/components/RemainingComponents.jsx
@@ -56,7 +56,7 @@ export const AboutBook = () => (
         <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
           <iframe
             style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-            src="https://www.youtube.com/embed/VIDEO_ID_AQUÍ"
+            src="https://www.youtube.com/embed/vDMdczIxJ9s"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
